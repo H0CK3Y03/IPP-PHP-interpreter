cb5f35acb08747496a049aafe0948a0fac745317
Adam Vesely <adamvesely2003@gmail.com>
2025-04-27 21:13:57 +0200

    ClassDefinition done

--- student/ClassDefinition.php
+++ student/ClassDefinition.php
@@ -2,20 +2,49 @@
 
 namespace IPP\Student;
 
+/**
+ * Represents a class definition with methods and an optional parent class.
+ */
 class ClassDefinition
 {
+    /**
+     * Name of the class.
+     *
+     * @var string
+     */
     public string $name;
+
+    /**
+     * Name of the parent class.
+     *
+     * @var string
+     */
     public string $parent;
 
-    /** @var array<string, Method> */
-    public $methods = [];
+    /**
+     * List of methods defined in this class, indexed by selector name.
+     *
+     * @var array<string, Method>
+     */
+    public array $methods = [];
 
-    public function __construct(string $name, string $parent_name)
+    /**
+     * ClassDefinition constructor.
+     *
+     * @param string $name Name of the class.
+     * @param string $parentName Name of the parent class.
+     */
+    public function __construct(string $name, string $parentName)
     {
         $this->name = $name;
-        $this->parent = $parent_name;
+        $this->parent = $parentName;
     }
-    // Add method to $this class
+
+    /**
+     * Adds a method to the class definition.
+     *
+     * @param Method $method The method to add.
+     */
     public function addMethod(Method $method): void
     {
         $this->methods[$method->selector_name] = $method;
