44cfc7954caed28caf2c36e26515fde7c2268e85
Adam Vesely <adamvesely2003@gmail.com>
2025-04-27 21:27:28 +0200

    Program complete

--- student/Program.php
+++ student/Program.php
@@ -2,12 +2,23 @@
 
 namespace IPP\Student;
 
+/**
+ * Represents a collection of class definitions forming the program.
+ */
 class Program
 {
-    /** @var array<string, AstClassDefinition> */
-    public $classes = [];
+    /**
+     * List of all class definitions in the program, indexed by class name.
+     *
+     * @var array<string, ClassDefinition>
+     */
+    public array $classes = [];
 
-    // Add a class definition to the program
+    /**
+     * Adds a class definition to the program.
+     *
+     * @param ClassDefinition $class
+     */
     public function addClass(ClassDefinition $class): void
     {
         $this->classes[$class->name] = $class;
