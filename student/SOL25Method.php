b5c04b723109d69cee836579b393bafb3c5079a8
Adam Vesely <adamvesely2003@gmail.com>
2025-04-28 00:50:18 +0200

    SOL25Method done

--- student/SOL25Method.php
+++ student/SOL25Method.php
@@ -6,13 +6,22 @@ use IPP\Student\Block;
 
 class SOL25Method
 {
+    // The name of the method
     public string $name;
+    
+    // The block of code associated with the method
     public Block $block;
+    
+    // Associative array of parameters where the key is the param name and the value is the param type
     /** @var array<string, string> */
     public array $params = [];
 
     /**
-     * @param array<string, string> $params
+     * Constructor to initialize the method's name, block, and parameters.
+     *
+     * @param string $name The name of the method.
+     * @param Block $block The block associated with the method.
+     * @param array<string, string> $params The parameters of the method.
      */
     public function __construct(string $name, Block $block, array $params)
     {
@@ -21,27 +30,34 @@ class SOL25Method
         $this->params = $params;
     }
 
-    // Get the block of the method
+    /**
+     * Get the block of the method.
+     *
+     * @return Block The block of the method.
+     */
     public function getBlock(): Block
     {
         return $this->block;
     }
 
     /**
-     * Set the params for the method
-     * @param array<string, string> $params
+     * Set the parameters for the method.
+     * This will overwrite any existing parameters.
+     *
+     * @param array<string, string> $params The new parameters for the method.
      */
-    public function setparams(array $params): void
+    public function setParameters(array $params): void
     {
         $this->params = $params;
     }
 
     /**
-     * Get the names of the method params
-     * @return array<string, string>
+     * Get the names of the method parameters.
+     *
+     * @return array<string> The names of the method parameters.
      */
     public function getParameterNames(): array
     {
-        return $this->params;
+        return array_keys($this->params);
     }
 }
